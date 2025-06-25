b44eb195147f68ef77d334a0390f00f6ffd3e05b
BrissoX <ba.brisso@duocuc.cl>
2025-06-25 00:25:46 -0400

    se agregan los test y descripcion en modelo

--- src/main/java/NSP_TECH/REPORTE/model/reporte.java
+++ src/main/java/NSP_TECH/REPORTE/model/reporte.java
@@ -1,6 +1,6 @@
 package NSP_TECH.REPORTE.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
@@ -26,26 +26,26 @@ public class reporte {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ID_REPORTE")
-    @Schema(description="aa")
+    @Schema(description="identificador del reporte", example="1")
     private Long id_reporte;
 
     @Column(name="TITULO",nullable = false,length = 100)
-    @Schema(description="aa")
+    @Schema(description="titulo del reporte", example="reporte #1")
     private String titulo;
     
     @Column(name="DESCRIPCION",nullable = true,length = 500)
-    @Schema(description="aa")
+    @Schema(description="referencia de las caracteristicas del reporte", example="Descripción del reporte #1")
     private String descripcion;
 
     @Column(name="FECHA_GENERACION",nullable = true)
-    @Schema(description="aa")
-    private Date fecha_generacion;
+    @Schema(description="Fecha en la que se genera el reporte", example="")
+    private LocalDateTime fecha_generacion;
 
     @Column(name="GENERADO_POR",nullable = true,length = 100)
-    @Schema(description="aa")
+    @Schema(description="Persona que realiza el reporte", example="Bastian Brisso")
     private String generado_por;
 
     @Column(name="TIPO_REPORTE",nullable = true,length = 50)
-    @Schema(description="aa")
+    @Schema(description="clasificacion del reporte", example="ventas, soporte, inventario")
     private String tipo_reporte;
 }
---
+++ src/test/java/NSP_TECH/REPORTE/Service/reporteServiceTest.java
@@ -0,0 +1,128 @@
+package NSP_TECH.REPORTE.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+import NSP_TECH.REPORTE.model.reporte;
+import NSP_TECH.REPORTE.repository.reporteRepository;
+import NSP_TECH.REPORTE.services.reporteServices;
+
+public class reporteServiceTest {
+    
+    @Mock
+    private reporteRepository reporterepository;
+    
+    @InjectMocks
+    private reporteServices reporteservices;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+
+    
+    @Test
+    public void testBuscarTodo(){
+    java.util.List<reporte> lista = new  ArrayList<>();
+
+    reporte rpt1 = new reporte();
+    reporte rpt2 = new reporte();
+
+
+    rpt1.setId_reporte(11L);
+    rpt1.setTitulo("Ganancias");
+    rpt1.setDescripcion("ganancias de la sucursal");
+    rpt1.setFecha_generacion(LocalDateTime.now());
+    rpt1.setGenerado_por("Bastian");
+    rpt1.setTipo_reporte("%Ganancias%");
+
+    rpt2.setId_reporte(12L);
+    rpt2.setTitulo("Perdidas");
+    rpt2.setDescripcion("perdidas de la sucursal");
+    rpt2.setFecha_generacion(LocalDateTime.now());
+    rpt2.setGenerado_por("Bastian");
+    rpt2.setTipo_reporte("%Perdidas%");
+    
+
+    lista.add(rpt1);
+    lista.add(rpt2);
+
+    when(reporterepository.findAll()).thenReturn(lista);
+
+    java.util.List<reporte> resultadoBusqueda = reporteservices.BuscarTodoReporte();
+
+    assertEquals(2,resultadoBusqueda.size());
+    verify(reporterepository, times(1)).findAll();
+
+}
+
+    @Test
+    public void testBuscarUnReporte(){
+    reporte rpt = new reporte();
+
+    rpt.setId_reporte(11L);
+    rpt.setTitulo("Ganancias");
+    rpt.setDescripcion("ganancias de la sucursal");
+    rpt.setFecha_generacion(LocalDateTime.now());
+    rpt.setGenerado_por("Bastian");
+    rpt.setTipo_reporte("%Ganancias%");
+
+    when(reporterepository.findById(11L)).thenReturn(Optional.of(rpt));
+
+    reporte reporteBuscado = reporteservices.BuscarUnReporte(11L);
+    assertEquals(11L,reporteBuscado.getId_reporte());
+    verify(reporterepository, times(1)).findById(11L);
+
+    }
+
+
+
+    @Test
+    public void testGuardarReporte(){
+        reporte r = new reporte();
+
+        r.setId_reporte(11L);
+        r.setTitulo("Ganancias");
+        r.setDescripcion("ganancias de la sucursal");
+        r.setFecha_generacion(LocalDateTime.now());
+        r.setGenerado_por("Bastian");
+        r.setTipo_reporte("%Ganancias%");
+
+        when(reporterepository.save(any())).thenReturn(r);
+
+        reporte reporteGuardados = reporteservices.GuardarReporte(r);
+        assertEquals(11L, reporteGuardados.getId_reporte());
+        verify(reporterepository, times(1)).save(r);
+
+    }
+
+
+/*
+
+    @Test
+    public void testEliminarEnvio(){
+        Long id = 11L;
+        doNothing().when(enviorepository).deleteById(id);
+
+        enviosservices.Eliminar***(id);
+
+        verify(enviosrepository.times(1)).deleteById(id);
+
+    }
+*/
+}
+
